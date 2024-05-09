613fbc39a9d3331e6ed28e61a8bfe6446be356cb
vincenzomorelli0 <116071526+vincenzomorelli0@users.noreply.github.com>
2024-05-09 17:59:42 +0200

    Update install.py

--- install.py
+++ install.py
@@ -17,15 +17,15 @@ header = """
 """
 
 print (header)
-print "\033[1;36mOperating Systems Available:\033[1;36m "
-print "\n--------------------------"
-print "(1) Kali Linux / Ubuntu "
-print "--------------------------\n"
+print ("\033[1;36mOperating Systems Available:\033[1;36m ")
+print ("\n--------------------------")
+print ("(1) Kali Linux / Ubuntu ")
+print ("--------------------------\n")
 
 option = input("\033[0m[>] Select Operating System: \033[0m")
 
 if option == 1:
-    print "\033[1;33m[*] Loading...\033[0m"
+    print ("\033[1;33m[*] Loading...\033[0m")
     os.system('apt-get install python-pip')
     import pip
     install = os.system("apt-get update && apt-get install -y build-essential git")
@@ -34,7 +34,7 @@ if option == 1:
     os.system('apt-get install lib32ncurses5-dev')
     os.system('apt-get install libncurses5-dev')
     pip.main(["install", "netifaces", "scapy", "SpoofMAC", "pythonwhois", "readline", "BeautifulSoup", "requests", "mechanize", "google", "qrcode"])
-    print "\033[1;32m[!] Finished Installing! Run 'trity' to run program [!]\033[0m"
+    print ("\033[1;32m[!] Finished Installing! Run 'trity' to run program [!]\033[0m")
     sys.exit()
 else:
-    print "Whoops! Something went wrong!"
+    print ("Whoops! Something went wrong!")
