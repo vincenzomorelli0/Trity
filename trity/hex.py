802f389617cc04e4ce4267cc1b86ee1190ec1341
vincenzomorelli0 <116071526+vincenzomorelli0@users.noreply.github.com>
2024-05-09 19:32:55 +0200

    Update hex.py

--- trity/hex.py
+++ trity/hex.py
@@ -22,8 +22,8 @@ T  = '\033[93m' # tan
 def encode1():
     Str = raw_input(''+T+'' + color.UNDERLINE + 'String to encode>' + color.END)
     Str = Str.encode('hex','strict');
-    print ""+G+"Encoded: " + Str
+    print (""+G+"Encoded: " + Str)
 def decode1():
     Str = raw_input(''+T+'' + color.UNDERLINE + 'String to decode>' + color.END)
     Str = Str.decode('hex','strict');
-    print ""+G+"Decoded String: " + Str
+    print (""+G+"Decoded String: " + Str)
