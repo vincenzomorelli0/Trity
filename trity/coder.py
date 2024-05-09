10376e8e7e261e20b05073ae5e7bc182f4355c44
vincenzomorelli0 <116071526+vincenzomorelli0@users.noreply.github.com>
2024-05-09 19:30:02 +0200

    Update coder.py

--- trity/coder.py
+++ trity/coder.py
@@ -22,8 +22,8 @@ T  = '\033[93m' # tan
 def encode():
     Str = raw_input(''+T+'' + color.UNDERLINE + 'String to encode>' + color.END)
     Str = Str.encode('base64','strict');
-    print ""+G+"Encoded: " + Str
+    print (""+G+"Encoded: " + Str)
 def decode():
     Str = raw_input(''+T+'' + color.UNDERLINE + 'String to decode>' + color.END)
     Str = Str.decode('base64','strict');
-    print ""+G+"Decoded String: " + Str
+    print (""+G+"Decoded String: " + Str)
